467a5b8cf3cbae0e5a4c67bb1bdcf74d129f525a
Adrien Joly <adrien@whyd.com>
2017-07-13 14:13:15 +0200

    refacto: simpler runScriptOnDatabase()

--- whydJS/app/models/mongodb-shell-runner.js
+++ whydJS/app/models/mongodb-shell-runner.js
@@ -1,78 +1,82 @@
 var vm = require('vm');
 var async = require('async');
 
+function makeCallback(prefix, callback) {
+  return function(err, res) {
+    //console.log(prefix, '=>', err ? err.errmsg : 'ok');
+    callback();
+  };
+}
+
+function buildContext(db, nextCommand, callback) {
+
+  function wrapCollection(colName, callback, commandCallback) {
+    db.collection(colName, function(err, col) {
+      if (err) {
+        callback(err);
+        return;
+      }
+      callback(null, {
+        dropIndex: function() {
+          var args = Array.prototype.slice.call(arguments).concat([
+            makeCallback(colName + '.dropIndex', commandCallback)
+          ]);
+          return col.dropIndex.apply(col, args)
+        },
+        ensureIndex: function() {
+          var args = Array.prototype.slice.call(arguments).concat([
+            makeCallback(colName + '.ensureIndex', commandCallback)
+          ]);
+          return col.ensureIndex.apply(col, args)
+        },
+      });
+    });
+  }
+
+  var shellDb = {
+    createCollection: function(colName) {
+      db.createCollection(colName, {}, makeCallback('db.createCollection', nextCommand));
+    },
+  };
+
+  db.collections(function(err, collections) {
+    if (err) throw err;
+    async.eachSeries(collections, function(colObj, nextCollection) {
+      // for each collection:
+      wrapCollection(colObj.collectionName, function(err, res) {
+        if (err) throw err;
+        shellDb[colObj.collectionName] = res;
+        nextCollection();
+      }, nextCommand);
+    }, function(err, res) {
+      callback(err, {
+        print: function() {
+          var args = ['[mongo shell]'].concat(Array.prototype.slice.call(arguments));
+          console.log.apply(console, args);
+          nextCommand();
+        },
+        db: shellDb,
+      });
+    });
+  });
+}
+
 // this method runs the commands of a mongo shell script (e.g. initdb.js)
 exports.runScriptOnDatabase = function(script, db, callback) {
-
   var commands = script.toString().split(/[\;\n]/);
   async.eachSeries(commands, function(command, nextCommand){
-
     command = command.trim();
-
     if (!command || /^\/\//.test(command) || /^\/\*.*\*\/$/.test(command)) {
       //console.log('IGNORE', command);
       nextCommand();
       return;
     } else {
       //console.log('RUN', command);
-    }
-
-    function makeCallback(prefix, callback) {
-      return function(err, res) {
-        //console.log(prefix, '=>', err ? err.errmsg : 'ok');
-        callback();
-      };
-    }
-    function wrapCollection(colName, callback, commandCallback) {
-      db.collection(colName, function(err, col) {
-        if (err) {
-          callback(err);
-          return;
-        }
-        callback(null, {
-          dropIndex: function() {
-            var args = Array.prototype.slice.call(arguments).concat([
-              makeCallback(colName + '.dropIndex', commandCallback)
-            ]);
-            return col.dropIndex.apply(col, args)
-          },
-          ensureIndex: function() {
-            var args = Array.prototype.slice.call(arguments).concat([
-              makeCallback(colName + '.ensureIndex', commandCallback)
-            ]);
-            return col.ensureIndex.apply(col, args)
-          },
-        });
-      });
-    }
-
-    var shellDb = {
-      createCollection: function(colName) {
-        db.createCollection(colName, {}, makeCallback('db.createCollection', nextCommand));
-      },
-    };
-
-    db.collections(function(err, collections) {
-      if (err) throw err;
-      async.eachSeries(collections, function(colObj, nextCollection) {
-        // for each collection:
-        wrapCollection(colObj.collectionName, function(err, res) {
-          if (err) throw err;
-          shellDb[colObj.collectionName] = res;
-          nextCollection();
-        }, nextCommand);
-      }, function() {
+      buildContext(db, nextCommand, function(err, context) {
         // when all collections are wrapped => run the current command
-        new vm.Script(command).runInContext(vm.createContext({
-          print: function() {
-            var args = ['[mongo shell]'].concat(Array.prototype.slice.call(arguments));
-            console.log.apply(console, args);
-            nextCommand();
-          },
-          db: shellDb,
-        }));
+        if (err) throw(err);
+        new vm.Script(command).runInContext(vm.createContext(context));
       });
-    });
-
+    }
   }, callback);
 };
